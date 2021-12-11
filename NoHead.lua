091b57c2d1a025bed32780519268de5eb10781cf
Valery <51917480+dmitriyewich@users.noreply.github.com>
2021-12-11 04:11:55 +0300

    Update NoHead.lua

--- NoHead.lua
+++ NoHead.lua
@@ -1,14 +1,9 @@
-local ffi = require("ffi")
-
 function main()
 	repeat wait(0) until isSampfuncsLoaded()
-    repeat wait(0) until isSampAvailable()
-	
-	local active = false
+
 	sampRegisterChatCommand("nohead", function()
 		active = not active
-		if active then arg = 2 else arg = 1 end
-		ffi.cast("void (__thiscall *)(uint32_t, int, char)", 0x5F0140)(getCharPointer(PLAYER_PED), tonumber(arg), 0)
+		require("ffi").cast("void (__thiscall *)(uint32_t, int, char)", 0x5F0140)(getCharPointer(PLAYER_PED), tonumber(active and 2 or 1), 0)
 	end)
 	wait(-1)
 end
