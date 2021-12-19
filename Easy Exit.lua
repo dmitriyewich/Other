6abe77f5ca0d1da443a42bf93b1bf73b6a8ac40e
Valery <51917480+dmitriyewich@users.noreply.github.com>
2021-12-19 13:59:47 +0300

    Add files via upload

---
+++ Easy Exit.lua
@@ -0,0 +1,30 @@
+script_name('Easy Exit')
+script_properties('work-in-pause', 'forced-reloading-only')
+
+local ffi = require("ffi")
+require"lib.moonloader"
+ffi.cdef 'void __stdcall ExitProcess(unsigned int)'
+
+function main()
+	
+	local keydown, keyup = 0, 0
+
+	while true do wait(0)
+		if wasKeyPressed(VK_ESCAPE) then keydown = os.clock() end
+		if isKeyDown(VK_ESCAPE) then
+			keyup = os.clock()
+			active = true
+			if (keyup - keydown) >= 0.47 then ffi.C.ExitProcess(0) end
+		end
+		if  wasKeyReleased(VK_ESCAPE) then active = false end
+	end
+end
+
+
+function onWindowMessage(msg, wparam, lparam)
+	if msg == 0x100 or msg == 0x104 then
+		if wparam == 27 and active then	
+			consumeWindowMessage(true, true)
+		end
+	end
+end
