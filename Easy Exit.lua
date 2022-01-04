47461174fb9b680896f14a32266ac49fb88a1c90
Valery <51917480+dmitriyewich@users.noreply.github.com>
2022-01-04 13:58:58 +0300

    v1.01

--- Easy Exit.lua
+++ Easy Exit.lua
@@ -2,16 +2,24 @@ script_name('Easy Exit')
 script_properties('work-in-pause', 'forced-reloading-only')
 require"lib.moonloader"
 
+local ffi = require('ffi')
+
+ffi.cdef[[
+	void* GetCurrentProcess();
+    bool TerminateProcess(void* hProcess, unsigned int uExitCode);
+	void ExitProcess(unsigned int uExitCode);
+]]
+
 function main()
 	
 	local keydown, keyup = 0, 0
-
+	
 	while true do wait(0)
 		if wasKeyPressed(VK_ESCAPE) then keydown = os.clock() end
 		if isKeyDown(VK_ESCAPE) then
 			keyup = os.clock()
 			active = true
-			if (keyup - keydown) >= 0.47 then os.execute('taskkill /IM gta_sa.exe /F') end
+			if (keyup - keydown) >= 0.47 then ffi.C.ExitProcess(0) end
 		end
 		if  wasKeyReleased(VK_ESCAPE) then active = false end
 	end
