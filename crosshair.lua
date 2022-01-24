2fcd91567c6af0fd650c03e6f1955c9cda89c4f5
Valery <51917480+dmitriyewich@users.noreply.github.com>
2022-01-24 12:24:51 +0300

    v1.14

--- crosshair.lua
+++ crosshair.lua
@@ -1,8 +1,6 @@
 script_name('crosshair')
-require("lib.moonloader")
 local lmemory, memory = pcall(require, 'memory')
-script_properties('work-in-pause', 'forced-reloading-only')
-script_version("1.13")
+script_version("1.14")
 
 function main()
 	repeat wait(0) until memory.read(0xC8D4C0, 4, false) == 9
@@ -18,7 +16,7 @@ function main()
 		local test = memory.getint16((0xB6F19C + memory.getint8(0xB6F028 + 0x59) * 0x238) + 0x0C) ~= 4
 		if (--[[targetting or target_car or]] test) and sx >= 0 and sy >= 0 and sx < sw and sy < sh then
 			local pos, cam = {convertScreenCoordsToWorld3D(sx, sy, 700.0)}, {getActiveCameraCoordinates()}
-			local result, colpoint = processLineOfSight(cam[1], cam[2], cam[3], pos[1], pos[2], pos[3], true, true, true, true, false, false, false, false)
+			local result, colpoint = processLineOfSight(cam[1], cam[2], cam[3], pos[1], pos[2], pos[3], true, true, true, false, false, false, false, false)
 			if result and (colpoint.entityType == 2 or colpoint.entityType == 3) and getCharPointerHandle(colpoint.entity) ~= PLAYER_PED then
 				changeCrosshairColor("0xFF3300FF")
 			else
@@ -28,33 +26,17 @@ function main()
 	end
 end
 
-function fixed_camera_to_skin() -- проверка на приклепление камеры к скину
+function fixed_camera_to_skin() -- проверка на приклепление камеры к скину, мой аналог sampIsLocalPlayerSpawned
 	return (memory.read(getModuleHandle('gta_sa.exe') + 0x76F053, 1, false) >= 1 and true or false)
 end
 
 function changeCrosshairColor(rgba)
-    local r = bit.band(bit.rshift(rgba, 24), 0xFF)
-    local g = bit.band(bit.rshift(rgba, 16), 0xFF)
-    local b = bit.band(bit.rshift(rgba, 8), 0xFF)
-    local a = bit.band(rgba, 0xFF)
+    local r, g, b, a = bit.band(bit.rshift(rgba, 24), 0xFF), bit.band(bit.rshift(rgba, 16), 0xFF), bit.band(bit.rshift(rgba, 8), 0xFF), bit.band(rgba, 0xFF)
 
-    memory.setuint8(0x58E301, r, true)
-    memory.setuint8(0x58E3DA, r, true)
-    memory.setuint8(0x58E433, r, true)
-    memory.setuint8(0x58E47C, r, true)
+	local tbl, clr, k = {0x58E301, 0x58E3DA, 0x58E433, 0x58E47C, 0x58E2F6, 0x58E3D1, 0x58E42A, 0x58E473, 0x58E2F1, 0x58E3C8, 0x58E425, 0x58E466, 0x58E2EC, 0x58E3BF, 0x58E420, 0x58E461}, {r, g, b, a}, 1
 
-    memory.setuint8(0x58E2F6, g, true)
-    memory.setuint8(0x58E3D1, g, true)
-    memory.setuint8(0x58E42A, g, true)
-    memory.setuint8(0x58E473, g, true)
-
-    memory.setuint8(0x58E2F1, b, true)
-    memory.setuint8(0x58E3C8, b, true)
-    memory.setuint8(0x58E425, b, true)
-    memory.setuint8(0x58E466, b, true)
-
-    memory.setuint8(0x58E2EC, a, true)
-    memory.setuint8(0x58E3BF, a, true)
-    memory.setuint8(0x58E420, a, true)
-    memory.setuint8(0x58E461, a, true)
+	for i = 1, #tbl do
+		memory.setuint8(tbl[i], clr[k], true)
+		if i % 4 == 0 then k = k + 1 end
+	end
 end
