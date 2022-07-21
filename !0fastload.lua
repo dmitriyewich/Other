aa10063368cc15426daa36e9697e75d47c2cba65
Valery <51917480+dmitriyewich@users.noreply.github.com>
2022-07-21 18:58:27 +0300

    run game instantly

--- !0fastload.lua
+++ !0fastload.lua
@@ -1,6 +1,6 @@
 script_properties('work-in-pause', 'forced-reloading-only')
 -- thx gennariarmando and LINK/2012 and KepchiK
-local lmemory, memory = pcall(require, 'memory')	
+local lmemory, memory = pcall(require, 'memory')
 memory.setuint8(0x53E797, 0xEB, 1, true)
 memory.fill(0x53EB85, 0x90, 2, true)
 memory.fill(0x53E826, 0x90, 2, true)
@@ -9,9 +9,9 @@ memory.fill(0x748CE1, 0x90, 10, true)
 memory.setuint8(0xC8D4C0, 5, 1, true)
 
 memory.fill(0x747483, 0x90, 6, true)
-	
+
 memory.fill(0x748C9A, 0x90, 5, true)
-	
+
 memory.fill(0x748C23, 0x90, 5, true)
 memory.fill(0x748C2B, 0x90, 5, true)
 
@@ -24,10 +24,12 @@ if memory.getuint8(0x590A1D) == 0xBE then
 	memory.setuint8(0x590A1D, 0xE9, 1, true)
 	memory.setuint32(0x590A1E, 0x8D, 4, true)
 end
-if memory.getuint8(0x748C6B) == 0xC6 then
-	memory.fill(0x748C6B, 0x90, 7, true)
-elseif memory.getuint8(0x748CBB) == 0xC6 then
-	memory.fill(0x748CBB, 0x90, 7, true)
+if getModuleHandle('samp.dll') ~= 0 and getModuleHandle('SAMPFUNCS.asi') == 0 then
+	if memory.getuint8(0x748C6B) == 0xC6 then
+		memory.fill(0x748C6B, 0x90, 7, true)
+	elseif memory.getuint8(0x748CBB) == 0xC6 then
+		memory.fill(0x748CBB, 0x90, 7, true)
+	end
 end
 if memory.getuint8(0x590AF0) == 0xA1 then
 	memory.setuint8(0x590AF0, 0xE9, 1, true)
