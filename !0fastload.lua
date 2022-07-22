6a83e0ce25cf65121d182b2e16bf3af714245a9b
Valery <51917480+dmitriyewich@users.noreply.github.com>
2022-07-22 18:11:29 +0300

    run game instantly v1.0

--- !0fastload.lua
+++ !0fastload.lua
@@ -3,6 +3,12 @@ script_properties('work-in-pause', 'forced-reloading-only')
 local lmemory, memory = pcall(require, 'memory')
 
 if memory.getuint32(0xC8D4C0, true) < 9 then
+	memory.fill(0x748A8D, 0x90, 6, true) -- Allow Alt+Tab without pausing the game
+
+	memory.setint8(0x53BC78, 0x00, true) -- Disable MENU AFTER alt + tab
+
+	memory.setint8(0x74754A, 0xB8, true) -- // No DirectPlay dependency
+	memory.setint32(0x74754B, 0x900, true) -- // Increase compatibility for Windows 8+
 
 	if memory.getuint8(0x747483, true) == 0x89 then -- Initialize game state
 		memory.fill(0x747483, 0x90, 6, true)
@@ -12,26 +18,33 @@ if memory.getuint32(0xC8D4C0, true) < 9 then
 
 	memory.setuint32(0xC8D4C0, 5, true) -- Skip ads
 
-	memory.fill(0x748C23, 0x90, 5, true)
-
 	if memory.getuint8(0x748C2B) == 0xE8 then -- Legal info fade-in
 		memory.fill(0x748C2B, 0x90, 5, true)
 	elseif memory.getuint8(0x748C7B) == 0xE8 then
 		memory.fill(0x748C7B, 0x90, 5, true)
 	end
 
+	if memory.getuint8(0x5909AA, true) == 0xBE then -- Legal info
+		memory.setuint32(0x5909AB, 1, true)
+	end
+
+	if memory.getuint8(0x590A1D, true) == 0xBE then -- Legal info fade-out
+		memory.setuint8(0x590A1D, 0xE9, true)
+		memory.setuint32(0x590A1E, 0x8D, 4, true)
+	end
+
+	memory.fill(0x748C23, 0x90, 5, true)
+
 	memory.fill(0x748C9A, 0x90, 5, true)
+
+	memory.setfloat(0x590DA4 + 2, 1.0, true) -- loadscreentime
 
 	memory.fill(0x748CF1, 0x90, 10, true)
 
-	-- if memory.getuint8(0x5909AA, true) == 0xBE then -- Legal info
-		-- memory.setuint32(0x5909AB, 1, true)
-	-- end
-
-	-- if memory.getuint8(0x590A1D, true) == 0xBE then -- Legal info fade-out
-		-- memory.setuint8(0x590A1D, 0xE9, true)
-		-- memory.setuint32(0x590A1E, 0x8D, 4, true)
-	-- end
+	memory.fill(0x5905B4, 0x90, 5, true) -- Disable loading bar rendering
+	memory.fill(0x748CF6, 0x90, 5, true) -- Disable audio tune from loading screen
+	memory.fill(0x590AC0, 0xC3, 1, true) -- Disable Loading Screen Patch
+	memory.fill(0x590D9F, "C390909090", 5, true) -- Disable Loading Screen Patch
 
 	-- if memory.getuint8(0x748C6B) == 0xC6 then -- Show load game
 		-- memory.fill(0x748C6B, 0x90, 7, true)
