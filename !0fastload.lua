9fb61e8a928adbe19c76876324c911c82dd70b22
Valery <51917480+dmitriyewich@users.noreply.github.com>
2022-07-22 18:28:11 +0300

    run game instantly v1.0

--- !0fastload.lua
+++ !0fastload.lua
@@ -3,12 +3,12 @@ script_properties('work-in-pause', 'forced-reloading-only')
 local lmemory, memory = pcall(require, 'memory')
 
 if memory.getuint32(0xC8D4C0, true) < 9 then
-	memory.fill(0x748A8D, 0x90, 6, true) -- Allow Alt+Tab without pausing the game
+	-- memory.fill(0x748A8D, 0x90, 6, true) -- Allow Alt+Tab without pausing the game
 
-	memory.setint8(0x53BC78, 0x00, true) -- Disable MENU AFTER alt + tab
+	-- memory.setint8(0x53BC78, 0x00, true) -- Disable MENU AFTER alt + tab
 
-	memory.setint8(0x74754A, 0xB8, true) -- // No DirectPlay dependency
-	memory.setint32(0x74754B, 0x900, true) -- // Increase compatibility for Windows 8+
+	-- memory.setint8(0x74754A, 0xB8, true) -- // No DirectPlay dependency
+	-- memory.setint32(0x74754B, 0x900, true) -- // Increase compatibility for Windows 8+
 
 	if memory.getuint8(0x747483, true) == 0x89 then -- Initialize game state
 		memory.fill(0x747483, 0x90, 6, true)
@@ -17,6 +17,8 @@ if memory.getuint32(0xC8D4C0, true) < 9 then
 	end
 
 	memory.setuint32(0xC8D4C0, 5, true) -- Skip ads
+
+	memory.fill(0x561AF0, 0x90, 7, true) -- antipause
 
 	if memory.getuint8(0x748C2B) == 0xE8 then -- Legal info fade-in
 		memory.fill(0x748C2B, 0x90, 5, true)
@@ -35,7 +37,11 @@ if memory.getuint32(0xC8D4C0, true) < 9 then
 
 	memory.fill(0x748C23, 0x90, 5, true)
 
-	memory.fill(0x748C9A, 0x90, 5, true)
+	memory.fill(0x748C9A, 0x90, 5, true) -- Hook the copyright screen fading in/out and simulates that it has happened
+	memory.setint8(0x8D093C, 0, true) -- Previous splash index = copyright notice
+	memory.setfloat(0xBAB340, 0, true)-- Decrease timeSinceLastScreen, so it will change immediately
+	memory.setint8(0x8D093C, 1, true)--  First Loading Splash
+
 
 	memory.setfloat(0x590DA4 + 2, 1.0, true) -- loadscreentime
 
