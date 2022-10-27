093a001e653407600985ad3713bff6d093de4271
Valery <51917480+dmitriyewich@users.noreply.github.com>
2022-10-27 21:42:53 +0300

    run game instantly v1.12

--- !0fastload.lua
+++ !0fastload.lua
@@ -1,3 +1,4 @@
+-- thx gennariarmando and LINK/2012 and sa-network and ThirteenAG
 local lmemory, memory = pcall(require, 'memory')
 if memory.getuint32(0xC8D4C0, true) < 9 then
 	memory.fill(0x747483, 0x90, 6, true) -- Initialize game state US 1.0
@@ -6,7 +7,6 @@ if memory.getuint32(0xC8D4C0, true) < 9 then
 	if getModuleHandle("sampfuncs.asi") ~= 0 then
 		memory.fill(0x748C9A, 0x90, 5, true) -- Disable Copyright screen
 	end
-
 	memory.fill(0x748AA8, 0x90, 6, true)
 	memory.hex2bin('C705C0D4C80005000000', 0x748AA8, 10)
 	memory.fill(0x748C23, 0x90, 5, true)
