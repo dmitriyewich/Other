21660e63e5d9e93b80eab0fcc2f9e25709d75905
Valery <51917480+dmitriyewich@users.noreply.github.com>
2026-04-05 16:04:41 +0300

    1.5.3 no SAMPFUNCS, work R1, R2, R3-1, R4, R4-2, R5-2, DL-R1

--- ActiveHitpoints+.lua
+++ ActiveHitpoints+.lua
@@ -2,26 +2,59 @@ script_name("ActiveHitpoints+")
 script_author("dmitriyewich")
 script_url("https://vk.com/dmitriyewichmods")
 script_properties('work-in-pause', 'forced-reloading-only')
-script_version("1.5.2")
+script_version("1.5.3")
 
 local memory = require("memory")
 local ffi = require("ffi")
 
 local entryPoint = {
     [0x31DF13] = 'R1', [0x3195DD] = 'R2', [0xCC490] = 'R3',
-    [0xCC4D0] = 'R3-1', [0xCBCB0] = 'R4', [0xcbcd0] = 'R4-2', [0xFDB60] = 'DL-R1'
+    [0xCC4D0] = 'R3-1', [0xCBCB0] = 'R4', [0xcbcd0] = 'R4-2', [0xCBC90] = 'R5-2', [0xFDB60] = 'DL-R1'
 }
 
 local main_offsets = {
-    ['SAMP_INFO_OFFSET'] = {['R1'] = 0x21A0F8, ['R2'] = 0x21A100, ['R3-1'] = 0x26E8DC, ['R4'] = 0x26EA0C, ['R4-2'] = 0x26EA0C, ['DL-R1'] = 0x2ACA24},
-    ['SAMP_INFO_OFFSET_Pools'] = {['R1'] = 0x3CD, ['R2'] = 0x3C5, ['R3-1'] = 0x3DE, ['R4'] = 0x3DE, ['R4-2'] = 0x3DE, ['DL-R1'] = 0x3DE},
-    ['SAMP_INFO_OFFSET_Pools_Player'] = {['R1'] = 0x18, ['R2'] = 0x8, ['R3-1'] = 0x8, ['R4'] = 0x8, ['R4-2'] = 0x4, ['DL-R1'] = 0x8},
-    ['SAMP_SLOCALPLAYERID_OFFSET'] = {['R1'] = 0x4, ['R2'] = 0x0, ['R3-1'] = 0x2F1C, ['R4'] = 0xC, ['R4-2'] = 0x4, ['DL-R1'] = 0x0},
-    ['SAMP_PREMOTEPLAYER_OFFSET'] = {['R1'] = 0x2E, ['R2'] = 0x26, ['R3-1'] = 0x4, ['R4'] = 0x2E, ['R4-2'] = 0x1F8A, ['DL-R1'] = 0x26},
-    ['SAMP_REMOTEPLAYERDATA_OFFSET'] = {['R1'] = 0x0, ['R2'] = 0xC, ['R3-1'] = 0x0, ['R4'] = 0x10, ['R4-2'] = 0x10, ['DL-R1'] = 0x8},
-    ['SAMP_REMOTEPLAYERDATA_HEALTH_OFFSET'] = {['R1'] = 0x1BC, ['R2'] = 0x1BC, ['R3-1'] = 0x1B0, ['R4'] = 0x1B0, ['R4-2'] = 0x1B0, ['DL-R1'] = 0x1B0},
-    ['ID_Find'] = {['R1'] = 0x10420, ['R2'] = 0x104C0, ['R3-1'] = 0x13570, ['R4'] = 0x13890, ['R4-2'] = 0x138C0, ['DL-R1'] = 0x137C0},
+    ['SAMP_INFO_OFFSET'] = {['R1'] = 0x21A0F8, ['R2'] = 0x21A100, ['R3-1'] = 0x26E8DC, ['R4'] = 0x26EA0C, ['R4-2'] = 0x26EA0C, ['R5-2'] = 0x26EB94, ['DL-R1'] = 0x2ACA24},
+    ['SAMP_INFO_OFFSET_Pools'] = {['R1'] = 0x3CD, ['R2'] = 0x3C5, ['R3-1'] = 0x3DE, ['R4'] = 0x3DE, ['R4-2'] = 0x3DE, ['R5-2'] = 0x3DE, ['DL-R1'] = 0x3DE},
+    ['SAMP_INFO_OFFSET_Pools_Player'] = {['R1'] = 0x18, ['R2'] = 0x8, ['R3-1'] = 0x8, ['R4'] = 0x8, ['R4-2'] = 0x4, ['R5-2'] = 0x4, ['DL-R1'] = 0x8},
+    ['SAMP_SLOCALPLAYERID_OFFSET'] = {['R1'] = 0x4, ['R2'] = 0x0, ['R3-1'] = 0x2F1C, ['R4'] = 0xC, ['R4-2'] = 0x4, ['R5-2'] = 0x4, ['DL-R1'] = 0x0},
+    ['MAX_PLAYER_ID_STREAMED_ONLY_OFFSET'] = {['R1'] = 0x0, ['R2'] = 0x22, ['R3-1'] = 0x0, ['R4'] = 0x0, ['R4-2'] = 0x2F3A, ['R5-2'] = 0x2F3A, ['DL-R1'] = 0x22},
+    ['SAMP_PREMOTEPLAYER_OFFSET'] = { ['R1'] = 0x2E, ['R2'] = 0x26, ['R3-1'] = 0x4, ['R4'] = 0x2E, ['R4-2'] = 0x1F8A, ['R5-2'] = 0x1F8A, ['DL-R1'] = 0x26},
+    ['SAMP_REMOTEPLAYERDATA_OFFSET'] = {['R1'] = 0x0, ['R2'] = 0xC, ['R3-1'] = 0x0, ['R4'] = 0x10, ['R4-2'] = 0x10, ['R5-2'] = 0x10, ['DL-R1'] = 0x8},
+    ['SAMP_REMOTEPLAYERDATA_ACTOR'] = {['R1'] = 0x0, ['R2'] = 0x1C, ['R3-1'] = 0x0, ['R4'] = 0x1DD, ['R4-2'] = 0x1DD, ['R5-2'] = 0x1DD, ['DL-R1'] = 0x4},
+    ['GTA_PED_HANDLE'] = {['R1'] = 0x2A4, ['R2'] = 0x2A4, ['R3-1'] = 0x2A4, ['R4'] = 0x2A4, ['R4-2'] = 0x2A4, ['R5-2'] = 0x2A4, ['DL-R1'] = 0x2A4},
+    ['SAMP_REMOTEPLAYERDATA_HEALTH_OFFSET'] = {['R1'] = 0x1BC, ['R2'] = 0x1BC, ['R3-1'] = 0x1B0, ['R4'] = 0x1B0, ['R4-2'] = 0x1B0, ['R5-2'] = 0x1B0, ['DL-R1'] = 0x1B0},
+    ['SAMP_REMOTEPLAYERDATA_ARMOR_OFFSET'] = {['R1'] = 0x1B8, ['R2'] = 0x1AC, ['R3-1'] = 0x1AC, ['R4'] = 0x1AC, ['R4-2'] = 0x1AC, ['R5-2'] = 0x1AC, ['DL-R1'] = 0x1AC},
+    ['ID_Find'] = {['R1'] = 0x10420, ['R2'] = 0x104C0, ['R3-1'] = 0x13570, ['R4'] = 0x13890, ['R4-2'] = 0x138C0, ['R5-2'] = 0x138C0, ['DL-R1'] = 0x137C0},
 }
+local required_offsets = {
+    'SAMP_INFO_OFFSET',
+    'SAMP_INFO_OFFSET_Pools',
+    'SAMP_INFO_OFFSET_Pools_Player',
+    'SAMP_SLOCALPLAYERID_OFFSET',
+    'MAX_PLAYER_ID_STREAMED_ONLY_OFFSET',
+    'SAMP_PREMOTEPLAYER_OFFSET',
+    'SAMP_REMOTEPLAYERDATA_OFFSET',
+    'SAMP_REMOTEPLAYERDATA_ACTOR',
+    'GTA_PED_HANDLE',
+    'SAMP_REMOTEPLAYERDATA_HEALTH_OFFSET',
+    'SAMP_REMOTEPLAYERDATA_ARMOR_OFFSET',
+    'ID_Find',
+}
+
+local function hasRequiredOffsets(version)
+    if type(version) ~= 'string' then
+        return false
+    end
+
+    for i = 1, #required_offsets do
+        local offsets = main_offsets[required_offsets[i]]
+        if type(offsets) ~= 'table' or offsets[version] == nil then
+            return false
+        end
+    end
+
+    return true
+end
 
 local ID_Find, sampModule, currentVersion
 
@@ -30,12 +63,12 @@ function main()
     sampModule = getModuleHandle("samp.dll")
 
     repeat wait(100) until isSampLoadedLua()
-    if currentVersion == 'UNKNOWN' or currentVersion == 'R3' then
+    if currentVersion == 'UNKNOWN' or not hasRequiredOffsets(currentVersion) then
         return thisScript():unload()
     end
 
     repeat wait(100) until isSAMPInitialized()
-    ID_Find = ffi.cast("int (__thiscall *)(intptr_t, intptr_t)", sampModule + main_offsets.ID_Find[currentVersion])
+    ID_Find = ffi.cast("unsigned short (__thiscall *)(intptr_t, intptr_t)", sampModule + main_offsets.ID_Find[currentVersion])
 
     while true do
         wait(0)
@@ -93,42 +126,88 @@ function getHealth(id)
 end
 
 function getPedID(handle)
-    if handle == PLAYER_PED then return true, getLocalID() end
-    local id = ID_Find(getPedPool(), getCharPointer(handle))
-    return id ~= 65535, id
+    local REMOTE_PLAYER, PLAYER_DATA, SAMP_ACTOR, GTA_PED_HANDLE
+
+    if handle == PLAYER_PED then
+        return true, getLocalID()
+    end
+
+    local pedPool = getPedPool()
+    local MAX_PLAYER_ID = memory.getint32(
+        pedPool + main_offsets.MAX_PLAYER_ID_STREAMED_ONLY_OFFSET[currentVersion],
+        true
+    )
+
+    for i = 0, MAX_PLAYER_ID do
+        REMOTE_PLAYER = memory.getuint32(
+            pedPool + main_offsets.SAMP_PREMOTEPLAYER_OFFSET[currentVersion] + i * 4,
+            true
+        )
+
+        if REMOTE_PLAYER > 0 then
+            PLAYER_DATA = memory.getuint32(
+                REMOTE_PLAYER + main_offsets.SAMP_REMOTEPLAYERDATA_OFFSET[currentVersion],
+                true
+            )
+
+            if PLAYER_DATA > 0 then
+                SAMP_ACTOR = memory.getuint32(
+                    PLAYER_DATA + main_offsets.SAMP_REMOTEPLAYERDATA_ACTOR[currentVersion],
+                    true
+                )
+
+                if SAMP_ACTOR > 0 then
+                    GTA_PED_HANDLE = memory.getuint32(
+                        SAMP_ACTOR + main_offsets.GTA_PED_HANDLE[currentVersion],
+                        true
+                    )
+
+                    if GTA_PED_HANDLE > 0 and GTA_PED_HANDLE == handle then
+                        return true, i
+                    end
+                end
+            end
+        end
+    end
+
+    return false, -1
+end
+
+function lerpColor(t, fromR, fromG, fromB, toR, toG, toB)
+    if t < 0 then
+        t = 0
+    elseif t > 1 then
+        t = 1
+    end
+
+    return fromR + (toR - fromR) * t,
+           fromG + (toG - fromG) * t,
+           fromB + (toB - fromB) * t
 end
 
 function getColorByHP(hp)
     if hp <= 1 then
-        return 0, 0, 0 -- мёртв
+        return 0, 0, 0
     elseif hp <= 10 then
-        return unpack(test(10, hp, 0, 0, 0, 255, 0, 0))
+        return lerpColor(hp / 10, 0, 0, 0, 255, 0, 0)
     elseif hp <= 50 then
-        return unpack(test(50, hp, 255, 0, 0, 255, 255, 0))
+        return lerpColor((hp - 10) / 40, 255, 0, 0, 255, 255, 0)
     elseif hp <= 100 then
-        return unpack(test(100, hp, 255, 255, 0, 25, 255, 25))
+        return lerpColor((hp - 50) / 50, 255, 255, 0, 25, 255, 25)
     else
-        return 25, 255, 25 -- > 100
+        return 25, 255, 25
     end
 end
 
-function test(maxhp, curhp, fromR, fromG, fromB, toR, toG, toB)
-    local deltaR = math.round((toR - fromR) / maxhp, 2)
-    local deltaG = math.round((toG - fromG) / maxhp, 2)
-    local deltaB = math.round((toB - fromB) / maxhp, 2)
-    return {
-        fromR + curhp * deltaR,
-        fromG + curhp * deltaG,
-        fromB + curhp * deltaB
-    }
-end
-
-function math.round(num, idp)
-    local mult = 10^(idp or 0)
-    return math.floor(num * mult + 0.5) / mult
-end
-
 function set_triangle_color(r, g, b)
+    r = math.floor((tonumber(r) or 0) + 0.5)
+    g = math.floor((tonumber(g) or 0) + 0.5)
+    b = math.floor((tonumber(b) or 0) + 0.5)
+
+    if r < 0 then r = 0 elseif r > 255 then r = 255 end
+    if g < 0 then g = 0 elseif g > 255 then g = 255 end
+    if b < 0 then b = 0 elseif b > 255 then b = 255 end
+	
     local bytes = "90909090909090909090909090C744240E00000000909090909090909090909090909090C744240F0000000090B300"
     memory.hex2bin(bytes, 0x60BB41, #bytes / 2)
     memory.setint8(0x60BB52, r)
